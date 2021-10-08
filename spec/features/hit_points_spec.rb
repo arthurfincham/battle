556fc618b4a5daeeab3b593317fb0f4613cd0944
Arthur Fincham <arthurfincham@me.com>
2021-10-08 18:03:43 +0100

    refactor test looking for text in browser

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,11 +1,11 @@
-feature 'view hit points' do
+feature 'Hit Points' do
   scenario 'see player 2 hit points' do
     sign_in_and_play
-    expect(page).to have_content 'Arthur: 50HP'
+    expect(page).to have_text('Arthur 50 HP', normalize_ws: true)
   end
 
   scenario 'see my own hit points' do
     sign_in_and_play
-    expect(page).to have_content '50HP: Tom'
+    expect(page).to have_text('Tom 50 HP', normalize_ws: true)
   end
 end 
--- spec/features/setup_spec.rb
+++ spec/features/setup_spec.rb
@@ -1,6 +1,7 @@
 feature 'Entering Players' do
   scenario 'Can enter names, submit names, and see them on screen' do
     sign_in_and_play
-    expect(page).to have_content 'Tom vs. Arthur'
+    expect(page).to have_text('Arthur 50 HP', normalize_ws: true)
+    expect(page).to have_text('Tom 50 HP', normalize_ws: true)
   end
 end
